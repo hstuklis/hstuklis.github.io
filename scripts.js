89c9cacd8aee7a151386ade0023fbb566f5516a0
hstuklis <henry.stuklis@gmail.com>
2025-03-29 07:38:20 +1030

    Update

--- scripts.js
+++ scripts.js
@@ -1,24 +1,24 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const rssFeedColumn = document.getElementById('rss-feed-column');
-
-    const titleItem = document.createElement('h4');
-    titleItem.textContent = 'RSS Feeds';
-    rssFeedColumn.appendChild(titleItem);
-
     fetchRSSFeed('https://news.ycombinator.com/rss')
-
+    fetchRSSFeed('')
 });
 
 function fetchRSSFeed(url) {
     const rssFeedColumn = document.getElementById('rss-feed-column');
+    const boxItem = document.createElement('div');
+    boxItem.classList.add('box');
+    const title = document.createElement('h5');
+    title.textContent = `${url}`;
+    boxItem.appendChild(title);
     fetch(`https://api.rss2json.com/v1/api.json?rss_url=${encodeURIComponent(url)}`)
         .then(response => response.json())
         .then(data => {
             data.items.forEach(item => {
                 const rssItem = document.createElement('div');
                 rssItem.innerHTML = `<a href="${item.link}" target="_blank">${item.title}</a>`
-                rssFeedColumn.appendChild(rssItem)
+                boxItem.appendChild(rssItem)
             });
         })
         .catch(error => console.error('Error fetching RSS feed:', error));
+    rssFeedColumn.appendChild(boxItem);
 }
