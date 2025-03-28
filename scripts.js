46d0ecea1c3833c7f6fccf37b1ba9f23043f5683
hstuklis <48230018+hstuklis@users.noreply.github.com>
2025-03-28 15:37:00 +1030

    Feat: Started refining RSS Feed code and style

--- scripts.js
+++ scripts.js
@@ -1,23 +1,24 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const rssFeedContainer = document.getElementById('rss-feed-container');
-    const rssUrls = [
-        'https://news.ycombinator.com/rss'
-    ];
+    const rssFeedColumn = document.getElementById('rss-feed-column');
 
-    rssUrls.forEach(url => {
-        fetch(`https://api.rss2json.com/v1/api.json?rss_url=${encodeURIComponent(url)}`)
-            .then(response => response.json())
-            .then(data => {
-                data.items.forEach(item => {
-                    const rssItem = document.createElement('div');
-                    rssItem.classList.add('rss-item');
-                    rssItem.innerHTML = `
-                        <h3><a href="${item.link}" target="_blank">${item.title}</a></h3>
-                        <p>${item.description}</p>
-                    `;
-                    rssFeedContainer.appendChild(rssItem);
-                });
-            })
-            .catch(error => console.error('Error fetching RSS feed:', error));
-    });
+    const titleItem = document.createElement('h4');
+    titleItem.textContent = 'RSS Feeds';
+    rssFeedColumn.appendChild(titleItem);
+
+    fetchRSSFeed('https://news.ycombinator.com/rss')
+
 });
+
+function fetchRSSFeed(url) {
+    const rssFeedColumn = document.getElementById('rss-feed-column');
+    fetch(`https://api.rss2json.com/v1/api.json?rss_url=${encodeURIComponent(url)}`)
+        .then(response => response.json())
+        .then(data => {
+            data.items.forEach(item => {
+                const rssItem = document.createElement('div');
+                rssItem.innerHTML = `<a href="${item.link}" target="_blank">${item.title}</a>`
+                rssFeedColumn.appendChild(rssItem)
+            });
+        })
+        .catch(error => console.error('Error fetching RSS feed:', error));
+}
