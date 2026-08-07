939a14faff0f00961bb568d4c6aed1afaa9e5033
HStuklis <henry.stuklis@gmail.com>
2026-08-07 15:16:23 +0930

    More quotes

--- src/data/quotes.ts
+++ src/data/quotes.ts
@@ -5,10 +5,6 @@ export interface Quote {
 
 export const quotes: Quote[] = [
   {
-    text: "Any sufficiently advanced technology is indistinguishable from magic.",
-    reference: "Arthur C. Clarke"
-  },
-  {
     text: "Six are the formulas to heaven by violence, one that you have learned by studying these words.",
     reference: "The 36 Lessons of Vivec, Sermon 6"
   },
@@ -17,7 +13,11 @@ export const quotes: Quote[] = [
     reference: "The 36 Lessons of Vivec, Sermon 21"
   },
   {
-    text: "All language is based on meat.",
+    text: "All language is based on meat. Do not let the sophists fool you.",
+    reference: "The 36 Lessons of Vivec, Sermon 27"
+  },
+  {
+    text: "No word is true until it is eaten.",
     reference: "The 36 Lessons of Vivec, Sermon 27"
   },
   {
@@ -33,19 +33,27 @@ export const quotes: Quote[] = [
     reference: "Dagoth Ur"
   },
   {
+    text: "Welcome, Moon-and-Star, to this place where destiny is made.",
+    reference: "Dagoth Ur"
+  },
+  {
     text: "We are like the dreamer who dreams, and then lives inside the dream. But who is the dreamer?",
     reference: "Monica Bellucci (to Gordon Cole)"
   },
   {
-    text: "I don't need anything. I want.",
-    reference: "Mr. C"
+    text: "I have no idea where this will lead us, but I have a definite feeling it will be a place both wonderful and strange.",
+    reference: "Dale Cooper"
   },
   {
-    text: "We believe that we invent symbols. The truth is that they invent us.",
-    reference: "Autarch Severian the Lame"
+    text: "Diane, 7:30 am, February 24th. Entering town of Twin Peaks. Five miles south of the Canadian border, twelve miles west of the state line. Never seen so many trees in my life.",
+    reference: "Dale Cooper"
   },
   {
-    text: "Time turns our lies into truth.",
+    text: "Hawk. Electricity is humming. You hear it in the mountains and rivers. You see it dance among the seas and stars and glowing around the moon, but in these days, the glow is dying.",
+    reference: "The Log Lady"
+  },
+  {
+    text: "We believe that we invent symbols. The truth is that they invent us; we are their creatures, shaped by their hard, defining edges.",
     reference: "Autarch Severian the Lame"
   },
   {
@@ -56,12 +64,4 @@ export const quotes: Quote[] = [
     text: "The best offense is a good defense, but a bad defense is offensive.",
     reference: "Autarch Severian the Lame"
   },
-  {
-    text: "\"I see,\" said Silk, though he did not, in either sense.",
-    reference: "Patera Silk"
-  },
-  {
-    text: "No cut!",
-    reference: "Oreb 🐦"
-  }
 ];
